b3de6a9e66cdbb260077a268641ca4676b691a44
Mauricio Serna F <mauricioserf@gmail.com>
2017-06-11 23:34:01 -0500

    [Add] initial commit, Spring started

---
+++ src/main/java/me/mauricioserna/javaApi/JavaApiApplication.java
@@ -0,0 +1,12 @@
+package me.mauricioserna.javaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiApplication.class, args);
+	}
+}
